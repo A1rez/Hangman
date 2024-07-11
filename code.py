f3767d1bd392c64e03811a37098f5ee2ce3972ef
Jeyson Aires <161245148+A1rez@users.noreply.github.com>
2024-07-11 12:11:17 -0300

    Update code.py
    now it opens a window where the player can input the word

--- code.py
+++ code.py
@@ -1,4 +1,43 @@
-answer = input('Digite a palavra que deve ser adivinhada: ')
+import tkinter as tk
+
+
+def obter_palavra():
+    def confirmar():
+        nonlocal palavra
+        palavra = entry.get()
+        janela.destroy()
+
+    palavra = None
+    janela = tk.Tk()
+    janela.title("Entrada de Palavra")
+
+    # Container 1: Label
+    container1 = tk.Frame(janela)
+    container1.pack(pady=10)
+    label = tk.Label(container1, text="Digite a palavra que deve ser adivinhada")
+    label.pack()
+
+    # Container 2: Entry
+    container2 = tk.Frame(janela)
+    container2.pack(pady=10)
+    entry = tk.Entry(container2)
+    entry.pack()
+
+    # Container 3: Button
+    container3 = tk.Frame(janela)
+    container3.pack(pady=10)
+    botao_confirmar = tk.Button(container3, text="Confirmar", command=confirmar)
+    botao_confirmar.pack()
+
+    janela.mainloop()
+
+    return palavra
+
+
+
+
+
+answer = obter_palavra()
 char = len(answer)
 
 i = 0
