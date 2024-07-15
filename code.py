78b690216d51d080f206fe9fcd9f3579e533aad1
Jeyson Aires <161245148+A1rez@users.noreply.github.com>
2024-07-15 13:56:52 -0300

    Update code.py

--- code.py
+++ code.py
@@ -1,5 +1,49 @@
 import tkinter as tk
 
+def forca(screen, n, Shots, pg):
+    def confirmar():
+        nonlocal palavra
+        palavra = entry.get()
+        janela.destroy()
+    palavra = None
+    janela = tk.Tk()
+    janela.title("Jogo da forca")
+
+    container1 = tk.Frame(janela)
+    container1.pack(pady=10, padx=150)
+    label = tk.Label(container1, text="A palavra contem "+ str(n) +" letras!", font = ("Arial", 15))
+    label.pack()
+
+    container4 = tk.Frame(janela)
+    container4.pack(pady=5, padx=150)
+    label = tk.Label(container1, text="Você possui "+ str(Shots) +" chutes", font = ("Arial", 10))
+    label.pack()
+
+    container2 = tk.Frame(janela)
+    container2.pack(pady=15, padx=150)
+    label = tk.Label(container2, text=''.join(screen), font = ("Arial", 50))
+    label.pack()
+
+    container5 = tk.Frame(janela)
+    container5.pack(pady=5, padx=150)
+    label = tk.Label(container1, text= pg, font = ("Arial", 25))
+    label.pack()
+
+    # Container 2: Entry
+    container2 = tk.Label(janela, text = 'Palpite:',pady=5, font = ("Arial", 25))
+    container2.pack()
+    entry = tk.Entry(janela, bd=5)
+    entry.pack()
+
+    # Container 3: Button
+    container3 = tk.Frame(janela)
+    container3.pack(pady=10)
+    botao_confirmar = tk.Button(container3, text="Confirmar", font = ("Arial", 25), command=confirmar)
+    botao_confirmar.pack()
+
+    janela.mainloop()
+
+    return palavra
 
 def obter_palavra():
     def confirmar():
@@ -14,7 +58,7 @@ def obter_palavra():
     # Container 1: Label
     container1 = tk.Frame(janela)
     container1.pack(pady=10)
-    label = tk.Label(container1, text="Digite a palavra que deve ser adivinhada")
+    label = tk.Label(container1, text="Digite a palavra que deve ser adivinhada", font = ("Arial", 25))
     label.pack()
 
     # Container 2: Entry
@@ -39,6 +83,7 @@ def obter_palavra():
 
 answer = obter_palavra()
 char = len(answer)
+pastguess=[]
 
 i = 0
 screen = []
@@ -47,14 +92,10 @@ while i < char:
     i=i+1
 
 shots = 5
-print('A palavra contem ',char,' letras!\n')
 
 while shots > 0 :
-    print('Você possui ',shots,' chutes!')
-    print(''.join(screen))
-
-    guess = input('\nQual o seu palpite?\n')
-
+    guess = forca(screen,char,shots, pastguess)
+    pastguess.append(guess)
     i = 0
     c = 0
     while i < char:
